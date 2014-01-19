078bc0d2ec75fdd9a2b7d15e174db8f3002ae3b0
Eric GELOEN <geloen.eric@gmail.com>
2014-01-19 20:16:05 +0100

    [Express] Simplify server

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 var express = require('express');
-var http = require('http');
 var path = require('path');
-
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, 'dist')));
 
-http.createServer(app).listen(app.get('port'), function () {
+app.listen(app.get('port'), function () {
     console.log('Server listening on port ' + app.get('port'));
 });
